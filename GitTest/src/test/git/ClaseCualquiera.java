c6cadeb602c84975248cb46b418984c891852cb8
Arnaldo Trujillo <atrujillo92work@gmail.com>
2014-06-03 14:12:18 +0200

    pusheando

--- GitTest/src/test/git/ClaseCualquiera.java
+++ GitTest/src/test/git/ClaseCualquiera.java
@@ -3,9 +3,10 @@ package test.git;
 public class ClaseCualquiera {
 	private int atributoCualquiera;
 
-	
-	private int otroMetodo(){
-		return 0;};
+	private int otroMetodo() {
+		return 0;
+	};
+
 	private int otroAtributo;
 
 	public int miMetodo() {
